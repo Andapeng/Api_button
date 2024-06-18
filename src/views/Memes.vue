118d4fa3b250caa905c201aa81365af98a92c36b
DYS763 <2362028292@qq.com>
2024-06-18 17:40:29 +0800

    修改api表情cdn

--- src/views/Memes.vue
+++ src/views/Memes.vue
@@ -21,7 +21,7 @@ export default {
   setup() {
     const getUrl = (path: string) => {
       return process.env.NODE_ENV === 'production'
-        ? `https://cdn.jsdelivr.net/gh/pipilapilayu/Api_button@main/public/memes/${path}`
+        ? `/memes/${path}`
         : `/memes/${path}`
     }
 
